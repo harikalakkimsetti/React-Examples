02e78aa908f617eb52b664b56975c5b6cdcfa6a9
Harika <42130874+harikalakkimsetti@users.noreply.github.com>
2020-10-21 14:25:18 +0530

    Added mission Initialized

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ const AgentBond= ()=>{
   (context)=>(
     <Fragment>
       <h3>Agent Information</h3>
+      <h4>MISSION INITIALIZED</h4>
       <p>Mission Name: {context.data.mname}</p>
   <h2>Mission Status: {context.data.accept}</h2>
   <button onClick = {context.isMissionAccepted}> Click to Accept</button>
